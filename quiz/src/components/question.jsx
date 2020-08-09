2eac5c6addaea19f2a7b2b0f9978af8a9fdef7ee
Nichole Godwin <headiehg@mail.com>
2020-08-09 13:17:40 -0700

    some style

--- quiz/src/components/question.jsx
+++ quiz/src/components/question.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Question extends React.Component {
   render() {
     return (
-      <p className="question" tabIndex="0">
+      <p className="question-asked questions" tabIndex="0">
         {this.props.data2[this.props.id].question}
       </p>
 );}}
--- quiz/src/components/questionCount.jsx
+++ quiz/src/components/questionCount.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 class Count extends React.Component {
   render() {
     return (
-<div className="question-count">
-<span>{this.props.id1+1}</span>
+<div className="question-count questions">
+{this.props.id1+1}
 </div>
 );}}
 
